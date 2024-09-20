af34f4e4c367238efdbbdbc691732441461b8e59
Yeray Darias <ydarias@gmail.com>
2024-09-20 13:28:08 +0200

    Initial commit

---
+++ src/main/java/com/ydarias/recipes/my_recipes/MyRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.ydarias.recipes.my_recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ydarias/recipes/my_recipes/MyRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydarias.recipes.my_recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
