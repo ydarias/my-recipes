29784989936b353338d011a3a54f03dc2906dbf6
Yeray Darias <ydarias@gmail.com>
2024-09-23 11:43:03 +0200

    Removed Arrays.asList in favor of List.of

--- src/main/java/com/ydarias/recipes/myrecipes/repositories/InMemoryIngredientsRepository.java
+++ src/main/java/com/ydarias/recipes/myrecipes/repositories/InMemoryIngredientsRepository.java
@@ -1,7 +1,6 @@
 package com.ydarias.recipes.myrecipes.repositories;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
@@ -17,9 +16,9 @@ public class InMemoryIngredientsRepository implements ForPersistingIngredients {
     private List<Ingredient> internalIngredientsDictionary = new ArrayList<>();
 
     public InMemoryIngredientsRepository() {
-        internalIngredientsDictionary.add(Ingredient.build("23f3423f-3c38-48ec-afd9-0aceea05aa4d", "Lemon", Arrays.asList("JAN", "FEB", "MAR", "APR", "MAY")));
-        internalIngredientsDictionary.add(Ingredient.build("6ec213a1-9e1d-4a73-ba5f-dfc621102af9", "Onion", Arrays.asList("APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT")));
-        internalIngredientsDictionary.add(Ingredient.build("17edc0d1-5525-42d9-8d75-84c94996cd84", "Watermelon", Arrays.asList("JUN", "JUL", "AUG")));
+        internalIngredientsDictionary.add(Ingredient.build("23f3423f-3c38-48ec-afd9-0aceea05aa4d", "Lemon", List.of("JAN", "FEB", "MAR", "APR", "MAY")));
+        internalIngredientsDictionary.add(Ingredient.build("6ec213a1-9e1d-4a73-ba5f-dfc621102af9", "Onion", List.of("APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT")));
+        internalIngredientsDictionary.add(Ingredient.build("17edc0d1-5525-42d9-8d75-84c94996cd84", "Watermelon", List.of("JUN", "JUL", "AUG")));
     }
 
     @Override
--- src/test/java/com/ydarias/recipes/myrecipes/ingredients/IngredientsCatalogTests.java
+++ src/test/java/com/ydarias/recipes/myrecipes/ingredients/IngredientsCatalogTests.java
@@ -1,12 +1,10 @@
 package com.ydarias.recipes.myrecipes.ingredients;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.util.Arrays;
+import java.util.List;
 
-import com.ydarias.recipes.myrecipes.controllers.IngredientsController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -26,10 +24,10 @@ public class IngredientsCatalogTests {
 
     @Test
     void theCatalogGetsAnIngredientsPageFromTheRepository() {
-        var ingredients = Arrays.asList(
-                Ingredient.build("23f3423f-3c38-48ec-afd9-0aceea05aa4d", "Lemon", Arrays.asList("JAN", "FEB", "MAR", "APR", "MAY")),
-                Ingredient.build("6ec213a1-9e1d-4a73-ba5f-dfc621102af9", "Onion", Arrays.asList("APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT")),
-                Ingredient.build("17edc0d1-5525-42d9-8d75-84c94996cd84", "Watermelon", Arrays.asList("JUN", "JUL", "AUG")));
+        var ingredients = List.of(
+                Ingredient.build("23f3423f-3c38-48ec-afd9-0aceea05aa4d", "Lemon", List.of("JAN", "FEB", "MAR", "APR", "MAY")),
+                Ingredient.build("6ec213a1-9e1d-4a73-ba5f-dfc621102af9", "Onion", List.of("APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT")),
+                Ingredient.build("17edc0d1-5525-42d9-8d75-84c94996cd84", "Watermelon", List.of("JUN", "JUL", "AUG")));
 
         when(ingredientsRepository.getIngredients(1, 10)).thenReturn(ingredients);
 
@@ -40,8 +38,8 @@ public class IngredientsCatalogTests {
 
     @Test
     void theCatalogCreatesTheIngredientAtTheRepository() {
-        var newPear =  IngredientCreationCommand.build("Pear", Arrays.asList("JUL", "AUG", "SEP", "OCT", "NOV"));
-        var createdPear = Ingredient.build("8840e6d9-b11d-424f-91da-28c991f911eb", "Pear", Arrays.asList("JUL", "AUG", "SEP", "OCT", "NOV"));
+        var newPear =  IngredientCreationCommand.build("Pear", List.of("JUL", "AUG", "SEP", "OCT", "NOV"));
+        var createdPear = Ingredient.build("8840e6d9-b11d-424f-91da-28c991f911eb", "Pear", List.of("JUL", "AUG", "SEP", "OCT", "NOV"));
 
         when(ingredientsRepository.addIngredient(newPear)).thenReturn(createdPear);
 
